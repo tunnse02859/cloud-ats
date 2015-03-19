0eb397563e496f7ee8525fd1ea900d58ee6ddaa1
TrinhTV3 <trinhtv060392@gmail.com>
2015-03-19 15:05:50 +0700

    create testList, testRef, testSearch, testFind for tenantService class

--- services/organization/src/test/java/org/ats/services/organization/TenantServiceTestCase.java
+++ services/organization/src/test/java/org/ats/services/organization/TenantServiceTestCase.java
@@ -4,6 +4,7 @@
 package org.ats.services.organization;
 
 
+import org.ats.common.PageList;
 import org.ats.services.organization.entity.Tenant;
 import org.ats.services.organization.entity.fatory.ReferenceFactory;
 import org.ats.services.organization.entity.fatory.TenantFactory;
@@ -35,6 +36,9 @@ public class TenantServiceTestCase extends AbstractTestCase {
   /** .*/
   private ReferenceFactory<TenantReference> tenantRefFactory;
   
+  /** .*/ 
+  private TenantFactory tenantFactory;
+  
   @Override
   @BeforeMethod
   public void init() throws Exception {
@@ -43,6 +47,7 @@ public class TenantServiceTestCase extends AbstractTestCase {
     this.factory = this.injector.getInstance(TenantFactory.class);
     this.featureRefFactory = this.injector.getInstance(Key.get(new TypeLiteral<ReferenceFactory<FeatureReference>>(){}));
     this.tenantRefFactory = this.injector.getInstance(Key.get(new TypeLiteral<ReferenceFactory<TenantReference>>(){}));
+    this.tenantFactory = this.injector.getInstance(TenantFactory.class);
   }
   
   @Test
@@ -65,5 +70,72 @@ public class TenantServiceTestCase extends AbstractTestCase {
     
     service.delete(fsoft);
     Assert.assertEquals(0, service.count());
+  }
+  
+  @Test
+  public void testList() {
+    initTenant(53);
+    Assert.assertEquals(53, service.count());
+    
+    Tenant newTenant = tenantFactory.create("newTenant");
+    service.create(newTenant);
+    Assert.assertEquals(54, service.count());
+    
+    String id = newTenant.getId();
+    service.delete(id);
+    Assert.assertEquals(53, service.count());
+    
+  }
+  
+  @Test
+  public void testSearch() {
+    initTenant(53);
+    PageList<Tenant> list = service.search("Tenant2");
+    Assert.assertEquals(1, list.count());
+    
+    service.delete("Tenant3");
+    
+    list = service.search("Tenant3");
+    Assert.assertEquals(0, list.count());
+    
+  }
+  
+  @Test
+  public void testFindTenantInFeature() {
+    
+    initTenant(53);
+    PageList<Tenant> list = service.findIn("features", featureRefFactory.create("performance"));
+    
+    Assert.assertEquals(53, list.count());
+    
+    Tenant tenant = list.next().get(0);
+    Assert.assertEquals(3, tenant.getFeatures().size());
+  }
+  
+  @Test
+  public void testReference() {
+    
+    Tenant newTenant = tenantFactory.create("newTenant");
+    service.create(newTenant);
+    
+    TenantReference ref = tenantRefFactory.create(newTenant.getId());
+    newTenant = ref.get();
+    
+    Assert.assertEquals("newTenant", newTenant.getId());
+  }
+  
+  private void initTenant(int total) {
+    
+    for (int i = 1; i <= total; i ++) {
+      
+      Tenant tenant = tenantFactory.create("Tenant"+ i);
+      FeatureReference refFeature1 = featureRefFactory.create("performance");
+      FeatureReference refFeature2 = featureRefFactory.create("functional");
+      FeatureReference refFeature3 = featureRefFactory.create("organization");
+      
+      tenant.addFeature(refFeature1, refFeature2, refFeature3);
+      
+      service.create(tenant);
+    }
   }
 }
