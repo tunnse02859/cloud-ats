ef020ebd083759d8d6c4f237a3e5c1e9bf7bfa9c
TrinhTV3 <trinhtv060392@gmail.com>
2015-03-26 17:51:45 +0700

    create actor deleteFeature to trigger

---
+++ services/organization/src/main/java/org/ats/services/organization/event/DeleteFeatureActor.java
@@ -0,0 +1,82 @@
+package org.ats.services.organization.event;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
+import org.ats.common.MapBuilder;
+import org.ats.common.PageList;
+import org.ats.services.event.Event;
+import org.ats.services.organization.TenantService;
+import org.ats.services.organization.entity.Feature;
+import org.ats.services.organization.entity.Tenant;
+import org.ats.services.organization.entity.fatory.ReferenceFactory;
+import org.ats.services.organization.entity.reference.FeatureReference;
+
+import akka.actor.UntypedActor;
+
+import com.google.inject.Inject;
+
+public class DeleteFeatureActor extends UntypedActor {
+
+  @Inject
+  private TenantService tenantService;
+  
+  @Inject
+  private ReferenceFactory<FeatureReference> featureRefFactory;
+  
+  @Inject
+  private Logger logger;
+  
+  @Override
+  public void onReceive(Object message) throws Exception {
+
+    logger.info("Recieved event " + message);
+    if (message instanceof Event) {
+      
+      Event event = (Event) message;
+      if ("delete-feature".equals(event.getName())) {
+        
+        Feature feature = (Feature) event.getSource();
+        FeatureReference ref = featureRefFactory.create(feature.getId());
+        
+        process(ref);
+      } else if ("delete-feature-ref".equals(event.getName())) {
+        
+        FeatureReference ref = (FeatureReference) event.getSource();
+        process(ref);
+        
+      } else {
+        unhandled(message);
+      }
+    }
+  }
+  
+  private void process(FeatureReference ref) {
+    
+    PageList<Tenant> listTenant = tenantService.findIn("features", ref);
+    
+    listTenant.setSortable(new MapBuilder<String, Boolean>("created_date", true).build());
+    
+    List<Tenant> holder = new ArrayList<Tenant>();
+    
+    while(listTenant.hasNext()) {
+      
+      for (Tenant tenant : listTenant.next()) {
+        holder.add(tenant);
+        
+      }
+    }
+    for (Tenant tenant : holder) {
+      
+      tenant.removeFeature(ref);
+      tenantService.update(tenant);
+    }
+    
+    if (!"deadLetters".equals(getSender().path().name())) {
+      getSender().tell(ref, getSelf());
+    }
+    
+  }
+
+}
