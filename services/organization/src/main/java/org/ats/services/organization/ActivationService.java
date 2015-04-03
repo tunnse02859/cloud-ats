8bf5f414ca747e42821ef83d8cf9eed1bb3df18b
TrinhTV3 <trinhtv060392@gmail.com>
2015-04-03 09:42:40 +0700

    create activation service

---
+++ services/organization/src/main/java/org/ats/services/organization/ActivationService.java
@@ -0,0 +1,77 @@
+/**
+ * 
+ */
+package org.ats.services.organization;
+
+import java.util.logging.Logger;
+
+import org.ats.services.data.MongoDBService;
+import org.ats.services.organization.entity.Feature;
+import org.ats.services.organization.entity.Space;
+import org.ats.services.organization.entity.Tenant;
+import org.ats.services.organization.entity.User;
+
+import com.google.inject.Inject;
+import com.mongodb.DBCollection;
+
+/**
+ * @author TrinhTV3
+ *
+ * Email: TrinhTV3@fsoft.com.vn
+ */
+public class ActivationService {
+
+//  private final String COL_INACTIVE_FEATURE = "inactived-feature";
+//  private final String COL_INACTIVE_TENANT = "inactived-tenant";
+//  private final String COL_INACTIVE_SPACE = "inactived-space";
+//  private final String COL_INACTIVE_USER = "inactived-user";
+  
+  private DBCollection featureCol;
+  private DBCollection tenantCol;
+  private DBCollection userCol;
+  private DBCollection spaceCol;
+  private Logger logger;
+  
+  @Inject
+  public ActivationService(MongoDBService mongo, Logger logger) {
+    
+    this.featureCol = mongo.getDatabase().getCollection("inactived-feature");
+    this.tenantCol = mongo.getDatabase().getCollection("inactived-tenant");
+    this.userCol = mongo.getDatabase().getCollection("inactived-user");
+    this.spaceCol = mongo.getDatabase().getCollection("inactived-space");
+    
+    this.logger = logger;
+  }
+  
+  public void inActiveFeature(String id) {
+    
+  }
+  
+  public void inActiveFeature(Feature obj) {
+    
+  }
+  
+  public void inActiveTenant(String id) {
+    
+  }
+  
+  public void inActiveTenant(Tenant obj) {
+    
+  }
+  
+  public void inActiveSpace(Space obj) {
+    
+  }
+  
+  public void inActiveSpace(String id) {
+    
+  }
+  
+  public void inActiveUser(User obj) {
+    
+  }
+  
+  public void inActiveUser(String id) {
+    
+  }
+}
