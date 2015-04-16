caa70d89d50632e0ba61fec3131b71726fbbbfc6
haint <haithanh0809@gmail.com>
2015-04-16 10:32:08 +0700

    FK: StoreCurrrentURL

--- services/functional/src/main/java/org/ats/services/functional/action/StoreCurrentUrl.java
+++ services/functional/src/main/java/org/ats/services/functional/action/StoreCurrentUrl.java
@@ -3,11 +3,27 @@
  */
 package org.ats.services.functional.action;
 
-/**
- * @author TrinhTV3
- *
- * Email: TrinhTV3@fsoft.com.vn
- */
-public class StoreCurrentUrl {
+import java.io.IOException;
 
+/**
+ * @author <a href="mailto:haithanh0809@gmail.com">Nguyen Thanh Hai</a>
+ *
+ * Apr 14, 2015
+ */
+public class StoreCurrentUrl implements IAction {
+
+  private String name;
+  
+  public StoreCurrentUrl(String name) {
+    this.name = name;
+  }
+  
+  public String transform() throws IOException {
+    StringBuilder sb = new StringBuilder("String ").append(name).append(" = wd.getCurrentUrl();\n");
+    return sb.toString();
+  }
+
+  public String getAction() {
+    return "storeCurrentUrl";
+  }
 }
