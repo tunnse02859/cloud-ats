f09d69c0cda2ef0e3bd1ceca56c94630493c3b30
TrinhTV3 <trinhtv060392@gmail.com>
2015-04-17 14:13:56 +0700

    refactor test case for dragandDropElement

--- services/functional/src/test/java/org/ats/services/functional/action/InputTestCase.java
+++ services/functional/src/test/java/org/ats/services/functional/action/InputTestCase.java
@@ -37,13 +37,13 @@ public class InputTestCase {
     IDLocator dest = new IDLocator(new Value("i am dest", false));
     
     DragToAndDropElement action = new DragToAndDropElement(source, dest);
-    Assert.assertEquals(action.transform(), "new Actions(wd).dragAndDrop(wd.findElement(By.id(\"i am source\")), wd.findElement(By.{locator2By}({locator2}))).build().perform();\n");
+    Assert.assertEquals(action.transform(), "new Actions(wd).dragAndDrop(wd.findElement(By.id(\"i am source\")), wd.findElement(By.id(\"i am dest\")).build().perform();\n");
     
     source = new IDLocator(new Value("i am source", true));
     dest = new IDLocator(new Value("i am dest", true));
     
     action = new DragToAndDropElement(source, dest);
-    Assert.assertEquals(action.transform(), "new Actions(wd).dragAndDrop(wd.findElement(By.id(i am source)), wd.findElement(By.{locator2By}({locator2}))).build().perform();\n");
+    Assert.assertEquals(action.transform(), "new Actions(wd).dragAndDrop(wd.findElement(By.id(i am source)), wd.findElement(By.id(i am dest)).build().perform();\n");
   
   }
   
