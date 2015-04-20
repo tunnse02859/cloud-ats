71f0f80fb4736314ba86d1680d09b92f576c1a7c
haint <haithanh0809@gmail.com>
2015-04-20 17:36:54 +0700

    Input keywords unit-test rewrite

--- services/functional/src/test/java/org/ats/services/functional/action/InputTestCase.java
+++ services/functional/src/test/java/org/ats/services/functional/action/InputTestCase.java
@@ -5,11 +5,16 @@ package org.ats.services.functional.action;
 
 import java.io.IOException;
 
-import org.ats.services.functional.Value;
-import org.ats.services.functional.locator.IDLocator;
-import org.ats.services.functional.locator.LinkTextLocator;
+import org.ats.services.FunctionalModule;
+import org.ats.services.functional.ActionFactory;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
 
 /**
  * @author TrinhTV3
@@ -17,76 +22,132 @@ import org.testng.annotations.Test;
  * Email: TrinhTV3@fsoft.com.vn
  */
 public class InputTestCase {
+  
+  private ActionFactory actionFactory;
+  
+  private ObjectNode json;
+  
+  private ObjectNode locator;
+  
+  private ObjectNode targetLocator;
+  
+  @BeforeMethod
+  public void init() throws Exception {
+    Injector injector = Guice.createInjector(new FunctionalModule());
+    this.actionFactory = injector.getInstance(ActionFactory.class);
+    ObjectMapper mapper = new ObjectMapper();
+    this.json = mapper.createObjectNode();
+    this.locator = mapper.createObjectNode();
+    this.targetLocator = mapper.createObjectNode();
+  }
 
   @Test
   public void testDoubleClickElement() throws IOException {
-    IDLocator locator = new IDLocator(new Value("i am id", false));
+    json.put("type", "doubleClickElement");
+    locator.put("type", "id");
+    locator.put("value", "i am id");
+    json.put("locator", locator);
     
-    DoubleClickElement action = new DoubleClickElement(locator);
+    IAction action = actionFactory.createAction(json);
     Assert.assertEquals(action.transform(), "new Actions(wd).doubleClick(wd.findElement(By.id(\"i am id\"))).build().perform();\n");
-    
-    locator = new IDLocator(new Value("i am id", true));
-    action = new DoubleClickElement(locator);
-    Assert.assertEquals(action.transform(), "new Actions(wd).doubleClick(wd.findElement(By.id(i am id))).build().perform();\n");
-    
+
+    locator.put("value", "${value}");
+    json.put("locator", locator);
+    action = actionFactory.createAction(json);
+    Assert.assertEquals(action.transform(), "new Actions(wd).doubleClick(wd.findElement(By.id(value))).build().perform();\n");
   }
   
   @Test
   public void testDragToAndDropElement() throws IOException {
-    IDLocator source = new IDLocator(new Value("i am source", false));
-    IDLocator dest = new IDLocator(new Value("i am dest", false));
+    json.put("type", "dragToAndDropElement");
     
-    DragToAndDropElement action = new DragToAndDropElement(source, dest);
+    locator.put("type", "id");
+    locator.put("value", "i am source");
+    json.put("locator", locator);
+    
+    targetLocator.put("type", "id");
+    targetLocator.put("value", "i am dest");
+    json.put("targetLocator", targetLocator);
+    
+    IAction action = actionFactory.createAction(json);
     Assert.assertEquals(action.transform(), "new Actions(wd).dragAndDrop(wd.findElement(By.id(\"i am source\")), wd.findElement(By.id(\"i am dest\")).build().perform();\n");
     
-    source = new IDLocator(new Value("i am source", true));
-    dest = new IDLocator(new Value("i am dest", true));
+    locator.put("type", "id");
+    locator.put("value", "${source}");
+    json.put("locator", locator);
     
-    action = new DragToAndDropElement(source, dest);
-    Assert.assertEquals(action.transform(), "new Actions(wd).dragAndDrop(wd.findElement(By.id(i am source)), wd.findElement(By.id(i am dest)).build().perform();\n");
-  
+    targetLocator.put("type", "id");
+    targetLocator.put("value", "${dest}");
+    json.put("targetLocator", targetLocator);
+    
+    action = actionFactory.createAction(json);
+    Assert.assertEquals(action.transform(), "new Actions(wd).dragAndDrop(wd.findElement(By.id(source)), wd.findElement(By.id(dest)).build().perform();\n");
   }
   
   @Test
   public void testClearSelections() throws IOException {
-    IDLocator locator = new IDLocator(new Value("foo", false));
-    ClearSelections clearSelections = new ClearSelections(locator);
+    json.put("type", "clearSelections");
+    locator.put("type", "id");
+    locator.put("value", "foo");
+    json.put("locator", locator);
+    
+    IAction clearSelections = actionFactory.createAction(json);
     Assert.assertEquals(clearSelections.transform(), "new Select(wd.findElement(By.id(\"foo\"))).deselectAll();\n");
   }
   
   @Test
   public void testClickAndHoldElement() throws IOException {
-    IDLocator locator = new IDLocator(new Value("foo", false));
-    ClickAndHoldElement clickAndHoldElement = new ClickAndHoldElement(locator);
+    json.put("type", "clickAndHoldElement");
+    locator.put("type", "id");
+    locator.put("value", "foo");
+    json.put("locator", locator);
+    
+    IAction clickAndHoldElement = actionFactory.createAction(json);
     Assert.assertEquals(clickAndHoldElement.transform(), "new Actions(wd).clickAndHold(wd.findElement(By.id(\"foo\"))).build.perform();\n");
   }
   
   @Test
   public void testClickElement() throws IOException {
-    LinkTextLocator locator = new LinkTextLocator(new Value("i am a link", false));
-    ClickElement clickElement = new ClickElement(locator);
+    json.put("type", "clickElement");
+    locator.put("type", "link text");
+    locator.put("value", "i am a link");
+    json.put("locator", locator);
+    
+    IAction clickElement = actionFactory.createAction(json);
     Assert.assertEquals(clickElement.transform(), "wd.findElement(By.linkText(\"i am a link\")).click();\n");
   }
   
   @Test
   public void testMouseOverElement() throws IOException {
-    IDLocator locator = new IDLocator(new Value("foo", false));
-    MouseOverElement mouseOverElement = new MouseOverElement(locator);
+    json.put("type", "mouseOverElement");
+    locator.put("type", "id");
+    locator.put("value", "foo");
+    json.put("locator", locator);
+    
+    IAction mouseOverElement = actionFactory.createAction(json);
     Assert.assertEquals(mouseOverElement.transform(), "new Actions(wd).moveToElement(wd.findElement(By.id(\"foo\"))).build().perform();\n");
   }
   
   @Test
   public void testReleaseElement() throws IOException {
-    IDLocator locator = new IDLocator(new Value("foo", false));
-    ReleaseElement releaseElement = new ReleaseElement(locator);
+    json.put("type", "releaseElement");
+    locator.put("type", "id");
+    locator.put("value", "foo");
+    json.put("locator", locator);
+    
+    IAction releaseElement = actionFactory.createAction(json);
     Assert.assertEquals(releaseElement.transform(), "new Actions(wd).release(wd.findElement(By.id(\"foo\"))).build.perform();\n");
   }
   
   @Test
   public void testSendKeysToElement() throws IOException {
-    Value text = new Value("w00t", false);
-    IDLocator locator = new IDLocator(new Value("comments", false));
-    SendKeysToElement sendKeysToElement = new SendKeysToElement(locator, text);
+    json.put("type", "sendKeysToElement");
+    json.put("text", "w00t");
+    locator.put("type", "id");
+    locator.put("value", "comments");
+    json.put("locator", locator);
+    
+    IAction sendKeysToElement = actionFactory.createAction(json); 
     Assert.assertEquals(sendKeysToElement.transform(), 
     "wd.findElement(By.id(\"comments\")).click();\n" +
     "wd.findElement(By.id(\"comments\")).sendKeys(\"w00t\");\n");
@@ -94,8 +155,12 @@ public class InputTestCase {
   
   @Test
   public void testSetElementNotSelected() throws IOException {
-    IDLocator locator = new IDLocator(new Value("unchecked_checkbox", false));
-    SetElementNotSelected setElementNotSelected = new SetElementNotSelected(locator);
+    json.put("type", "setElementNotSelected");
+    locator.put("type", "id");
+    locator.put("value", "unchecked_checkbox");
+    json.put("locator", locator);
+    
+    IAction setElementNotSelected = actionFactory.createAction(json);
     Assert.assertEquals(setElementNotSelected.transform(),
             "if (wd.findElement(By.id(\"unchecked_checkbox\")).isSelected()) {\n" +
             "wd.findElement(By.id(\"unchecked_checkbox\")).click();\n" +
@@ -104,8 +169,12 @@ public class InputTestCase {
   
   @Test
   public void testSetElementSelected() throws IOException {
-    IDLocator locator = new IDLocator(new Value("unchecked_checkbox", false));
-    SetElementSelected selected = new SetElementSelected(locator);
+    json.put("type", "setElementSelected");
+    locator.put("type", "id");
+    locator.put("value", "unchecked_checkbox");
+    json.put("locator", locator);
+
+    IAction selected = actionFactory.createAction(json);
     Assert.assertEquals(selected.transform(),
     "if (!wd.findElement(By.id(\"unchecked_checkbox\")).isSelected()) {\n" +
       "wd.findElement(By.id(\"unchecked_checkbox\")).click();\n" +
@@ -114,9 +183,13 @@ public class InputTestCase {
   
   @Test
   public void testSetElementText() throws IOException {
-    Value text = new Value("foo", false);
-    IDLocator locator = new IDLocator(new Value("unchecked_checkbox",false));
-    SetElementText setElementText = new SetElementText(locator, text);
+    json.put("type", "setElementText");
+    json.put("text", "foo");
+    locator.put("type", "id");
+    locator.put("value", "unchecked_checkbox");
+    json.put("locator", locator);
+    
+    IAction setElementText = actionFactory.createAction(json);
     Assert.assertEquals(setElementText.transform(), 
     "wd.findElement(By.id(\"unchecked_checkbox\")).click();\n" +
     "wd.findElement(By.id(\"unchecked_checkbox\")).clear();\n" +
@@ -126,8 +199,12 @@ public class InputTestCase {
   
   @Test
   public void testSubmitElement() throws IOException {
-    IDLocator locator = new IDLocator(new Value("comments", false));
-    SubmitElement submitElement = new SubmitElement(locator);
+    json.put("type", "submitElement");
+    locator.put("type", "id");
+    locator.put("value", "comments");
+    json.put("locator", locator);
+    
+    IAction submitElement = actionFactory.createAction(json);
     Assert.assertEquals(submitElement.transform(), "wd.findElement(By.id(\"comments\")).submit();\n");
   }
   
