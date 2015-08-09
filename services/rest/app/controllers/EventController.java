81919510c28552bc47cc4d65d8f73e87b54fb9b7
haint <haithanh0809@gmail.com>
2015-08-09 15:49:58 +0700

    Keyword event job tracking

--- services/rest/app/controllers/EventController.java
+++ services/rest/app/controllers/EventController.java
@@ -11,14 +11,14 @@ import org.ats.services.executor.job.AbstractJob;
 import org.ats.services.organization.base.AuthenticationService;
 import org.ats.services.organization.entity.User;
 
-import com.google.inject.Inject;
-
 import play.Logger;
 import play.libs.EventSource;
 import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.Result;
 import actions.CorsComposition;
+
+import com.google.inject.Inject;
 
 /**
  * @author <a href="mailto:haithanh0809@gmail.com">Nguyen Thanh Hai</a>
@@ -33,6 +33,18 @@ public class EventController extends Controller {
   @Inject AuthenticationService<User> authenService;
   
   public void send(User user, AbstractJob<?> job) {
+    
+    //Send for everyone
+    if (user == null) {
+     Logger.info("Send to all user");
+     for (List<EventSource> list : pool.values()) {
+       for (EventSource event : list) {
+         event.send(EventSource.Event.event(Json.parse(job.toString())));
+       }
+     }
+     return;
+    }
+    
     String token = authenService.createToken(user);
     
     List<EventSource> events = pool.get(token);
@@ -51,18 +63,10 @@ public class EventController extends Controller {
       event.close();
     }
     pool.remove(token);
-    System.out.println("close");
     return status(200);
   }
 
   public Result feed(final String token) {
-    final String remoteAddress = request().remoteAddress();
-    Logger.info(remoteAddress + " - SSE connected");
-   
-    if (pool.get(token) != null) {
-      System.out.println(pool.get(token).size());
-    }
-    
     return ok(new EventSource() {
       @Override
       public void onConnected() {
@@ -71,7 +75,6 @@ public class EventController extends Controller {
         
         List<EventSource> events = pool.get(token) == null ? new ArrayList<EventSource>() : pool.get(token);
         events.add(currentSocket);
-        
         pool.put(token, events);
       }
     });
