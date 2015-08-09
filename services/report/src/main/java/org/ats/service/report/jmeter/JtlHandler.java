2dde87006f6e66dd0975cb44973a87a3a115b2d3
tuanhq <hoangquoctuan1985@gmail.com>
2015-08-09 14:17:08 +0700

    chang parsing report from xml to csv

--- services/report/src/main/java/org/ats/service/report/jmeter/JtlHandler.java
+++ services/report/src/main/java/org/ats/service/report/jmeter/JtlHandler.java
@@ -1,12 +1,14 @@
 package org.ats.service.report.jmeter;
 
 import java.io.ByteArrayInputStream;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.Scanner;
 import java.util.TreeMap;
 
 import javax.xml.parsers.ParserConfigurationException;
@@ -24,8 +26,15 @@ import com.google.inject.Inject;
 import com.google.inject.assistedinject.Assisted;
 
 public class JtlHandler extends DefaultHandler {
-  
-  private String parsingType = "STRING";
+
+  public static enum FileContentType {
+    CSV, XMl;
+  }
+
+  public static enum ParsingType {
+    STRING, FILE;
+  }
+
   private String urlTotal = "*SummaryReport*";
   private Map<String, Report> totalUrlMap = new HashMap<String, Report>();
   private String content = "";
@@ -37,39 +46,111 @@ public class JtlHandler extends DefaultHandler {
   private Report reportUrl;
   private boolean isInAssertion = false;
   private boolean failureValue = true;
-  private boolean errorValue = true;
+  private boolean errorValue = true;  
+ 
 
   @Inject
-  public JtlHandler(ExecutorService executorService, @Assisted("performaneJobId") String performaneJobId,  @Assisted("scriptId") String scriptId) {
+  public JtlHandler(ExecutorService executorService, @Assisted("performaneJobId") String performaneJobId, @Assisted("scriptId") String scriptId) {
     this.executorService = executorService;
     this.performaneJobId = performaneJobId;
     this.scriptId = scriptId;
     PerformanceJob job = (PerformanceJob) this.executorService.get(performaneJobId);
     Iterator<Map.Entry<String, String>> iterator = job.getRawDataOutput().entrySet().iterator();
-    while(iterator.hasNext()){
+    while (iterator.hasNext()) {
       Map.Entry<String, String> entry = iterator.next();
-      if(entry.getKey().equals(scriptId)){
+      if (entry.getKey().equals(scriptId)) {
         this.content = entry.getValue();
         break;
       }
     }
-    
+
   }
 
-  public void startParsing() throws SAXException, IOException, ParserConfigurationException {
+  public void startParsing(ParsingType parsingType, FileContentType fileContentType) throws Exception {
     if (content.isEmpty()) {
       throw new ParserConfigurationException("Error content parsing is empty");
     }
-    if ("STRING".equalsIgnoreCase(parsingType)) {
+
+    if (FileContentType.XMl.equals(fileContentType)) {
       SAXParserFactory factory = SAXParserFactory.newInstance();
       SAXParser saxParser = factory.newSAXParser();
-      InputStream ins = new ByteArrayInputStream(content.getBytes(StandardCharsets.UTF_8));
-      saxParser.parse(ins, this);
-      try {
-        ins.close();
-      } catch (Exception e) {
-        throw e;
+      if (parsingType.equals(ParsingType.STRING)) {
+        InputStream ins = new ByteArrayInputStream(content.getBytes(StandardCharsets.UTF_8));
+        saxParser.parse(ins, this);
+        try {
+          ins.close();
+        } catch (Exception e) {
+          throw e;
+        }
+      } else if (parsingType.equals(ParsingType.FILE)) {
+        saxParser.parse(content, this);
+      } else {
+
       }
+
+    } else if (FileContentType.CSV.equals(fileContentType)) {
+      if (parsingType.equals(ParsingType.STRING)) {
+        Scanner sc = null;
+        try {
+          boolean isFirstLine = true;
+          sc = new Scanner(content);
+          Map<Integer, String> columnNameHashMap = null;
+          Map<String, String> valueHashMap;
+          ;
+          while (sc.hasNext()) {
+            String line = sc.nextLine();
+            String[] values = line.split(",");
+            if (isFirstLine) {
+              columnNameHashMap = Calculation.calculateIndexOfHashMap(values);
+              isFirstLine = false;
+            } else {
+              valueHashMap = Calculation.calculateCsvLine(columnNameHashMap, values);
+              startParsingLine(valueHashMap);
+              
+            }
+          }
+        } catch (Exception e) {
+          throw e;
+        } finally {
+          if (sc != null) {
+            sc.close();
+          }
+        }
+
+      } else if (parsingType.equals(ParsingType.FILE)) {
+        InputStream ins = new FileInputStream(content);
+        Scanner sc = null;
+        try {
+          boolean isFirstLine = true;
+          sc = new Scanner(ins, "UTF-8");
+          Map<Integer, String> columnNameHashMap = null;
+          Map<String, String> valueHashMap;
+          ;
+          while (sc.hasNext()) {
+            String line = sc.nextLine();
+            String[] values = line.split(",");
+            if (isFirstLine) {
+              columnNameHashMap = Calculation.calculateIndexOfHashMap(values);
+              isFirstLine = false;
+            } else {
+              valueHashMap = Calculation.calculateCsvLine(columnNameHashMap, values);
+              startParsingLine(valueHashMap);
+            }
+          }
+        } catch (Exception e) {
+          throw e;
+        } finally {
+          if (sc != null) {
+            sc.close();
+          }
+        }
+
+      
+
+      } else {
+
+      }
+
     }
 
   }
@@ -78,13 +159,42 @@ public class JtlHandler extends DefaultHandler {
     return totalUrlMap;
   }
 
+  public void parsingCSV() {
+
+  }
+
+  public void startParsingLine(Map<String, String> valueHashMap) {
+
+    HttpSamplerObj httpSamplerObj = new HttpSamplerObj(valueHashMap);
+    reportAll = totalUrlMap.get(urlTotal);
+    if (reportAll == null) {
+      reportAll = new Report(urlTotal, performaneJobId, null, scriptId);
+      reportAll.setSummaryReport(new SummaryReport(urlTotal));
+      reportAll.setTransPersecond(new TreeMap<Long, PointReport>());
+      reportAll.setHitPerSecond(new TreeMap<Long, PointReport>());
+      totalUrlMap.put(urlTotal, reportAll);
+    }
+    Calculation.calculateFromCsv(reportAll, httpSamplerObj);
+    String url = httpSamplerObj.getLable();
+    reportUrl = totalUrlMap.get(url);
+    if (reportUrl == null) {
+      reportUrl = new Report(url, performaneJobId, null, scriptId);
+      reportUrl.setSummaryReport(new SummaryReport(url));
+      reportUrl.setTransPersecond(new TreeMap<Long, PointReport>());
+      reportUrl.setHitPerSecond(new TreeMap<Long, PointReport>());
+      totalUrlMap.put(url, reportUrl);
+    }
+    Calculation.calculateFromCsv(reportUrl, httpSamplerObj);
+
+  }
+
   @Override
   public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
     if (qName.equalsIgnoreCase("httpSample")) {
       HttpSamplerObj httpSamplerObj = new HttpSamplerObj(attributes);
       reportAll = totalUrlMap.get(urlTotal);
       if (reportAll == null) {
-        reportAll = new Report(urlTotal, performaneJobId, null,scriptId );
+        reportAll = new Report(urlTotal, performaneJobId, null, scriptId);
         reportAll.setSummaryReport(new SummaryReport(urlTotal));
         reportAll.setTransPersecond(new TreeMap<Long, PointReport>());
         reportAll.setHitPerSecond(new TreeMap<Long, PointReport>());
@@ -94,7 +204,7 @@ public class JtlHandler extends DefaultHandler {
       String url = httpSamplerObj.getLable();
       reportUrl = totalUrlMap.get(url);
       if (reportUrl == null) {
-        reportUrl = new Report(url, performaneJobId, null,scriptId);
+        reportUrl = new Report(url, performaneJobId, null, scriptId);
         reportUrl.setSummaryReport(new SummaryReport(url));
         reportUrl.setTransPersecond(new TreeMap<Long, PointReport>());
         reportUrl.setHitPerSecond(new TreeMap<Long, PointReport>());
