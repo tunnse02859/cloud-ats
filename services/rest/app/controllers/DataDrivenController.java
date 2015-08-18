ff35fcabdac16975883c7839da152110aa03f0f8
haint <haithanh0809@gmail.com>
2015-08-18 16:37:27 +0700

    Update route

--- services/rest/app/controllers/DataDrivenController.java
+++ services/rest/app/controllers/DataDrivenController.java
@@ -3,10 +3,6 @@
  */
 package controllers;
 
-import java.util.List;
-
-import org.ats.common.MapBuilder;
-import org.ats.common.PageList;
 import org.ats.services.OrganizationContext;
 import org.ats.services.datadriven.DataDriven;
 import org.ats.services.datadriven.DataDrivenFactory;
@@ -19,16 +15,14 @@ import org.ats.services.organization.acl.Authenticated;
 import org.ats.services.organization.entity.fatory.ReferenceFactory;
 import org.ats.services.organization.entity.reference.SpaceReference;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.node.ArrayNode;
-import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.google.inject.Inject;
-import com.mongodb.BasicDBObject;
-
 import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.Result;
 import actions.CorsComposition;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.google.inject.Inject;
 
 /**
  * @author <a href="mailto:haithanh0809@gmail.com">Nguyen Thanh Hai</a>
@@ -63,24 +57,7 @@ public class DataDrivenController extends Controller {
   @Inject
   CaseService caseService;
   
-  public Result list(String tenant, String space) {
-    BasicDBObject query = new BasicDBObject("tenant", new BasicDBObject("_id", tenant));
-    query.append("space", "null".equals(space) ? null : new BasicDBObject("_id", space));
-    
-    PageList<DataDriven> list = service.query(query);
-    list.setSortable(new MapBuilder<String, Boolean>("created_date", true).build());
-    
-    ArrayNode array = Json.newObject().arrayNode();
-    while(list.hasNext()) {
-      List<DataDriven> page = list.next();
-      for (DataDriven item : page) {
-        array.add(Json.parse(item.toString()));
-      }
-    }
-    return ok(array);
-  }
-  
-  public Result newData() {
+  public Result create() {
     JsonNode json = request().body().asJson();
     String name = json.get("name").asText();
     String caseId = json.get("caseId").asText();
@@ -96,7 +73,7 @@ public class DataDrivenController extends Controller {
     return ok(Json.parse(driven.toString()));
   }
   
-  public Result getDataSet(String id) {
+  public Result get(String id) {
     
     DataDriven driven = service.get(id);
     ObjectNode json = Json.newObject();
